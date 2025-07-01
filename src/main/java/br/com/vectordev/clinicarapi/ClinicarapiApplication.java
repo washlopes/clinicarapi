02f684b6108569d020e7cde53dd43f3fda78d172
Washington Luís Pedrosa Lopes <washlopes@gmail.com>
2025-07-01 01:54:38 -0300

    Inicialização do projeto clinicarapi com spring boot

---
+++ src/main/java/br/com/vectordev/clinicarapi/ClinicarapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.vectordev.clinicarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicarapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicarapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vectordev/clinicarapi/ClinicarapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vectordev.clinicarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicarapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
